44a1c83d07fb7ce34b393a5a2d2f1584fa9ef06e
Oizys18 <oizys18@gmail.com>
2020-08-26 16:50:42 +0900

    Add ejs

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var express = require('express')
 var app = express()
-app.listen(3000,function(){
+var bodyParser = require('body-parser')
+
+app.listen(3000, function () {
     console.log('start: express server on port 3000')
 });
 
 // static 등록
-app.use(express.static('public'))
-
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs')
 
 // url routing 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/main.html")
 })
 
 // url routing : /main 
-app.get('/main',function(req,res){
+app.get('/main', function (req, res) {
     res.sendFile(__dirname + "/public/main.html")
+})
+// url routing : /main 
+// app.get('/form',function(req,res){
+//     res.sendFile(__dirname + "/public/form.html")
+// })
+
+app.post('/email_post', function (req, res) {
+    console.log(req.body.email)
+    res.render('email.ejs', {'email':req.body.email})
+    // res.send(`<h1>Welcome ${req.body.email}</h1>`)
 })
